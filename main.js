2dfae399b083d9a954225423a9de7537c31b5d04
10n3 <52797401+10n3@users.noreply.github.com>
2021-03-11 00:25:11 +0200

    Add files via upload for NodeJS course (lection)

---
+++ main.js
@@ -0,0 +1,7 @@
+const summ = require('./node_modules_math/summ');
+const abs = require('./node_modules_math/abs');
+const min = require('./node_modules_math/min');
+
+console.log(`Summ is: ${summ([1,2,3,4])}`);
+console.log(`Abs is: ${abs(-3)}`);
+console.log(`Min is: ${min([3, -8, 10, 15, 25, -15])}`);
